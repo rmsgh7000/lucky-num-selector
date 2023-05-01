3e631a6776082f762ab2a366e5f1b531b44394e5
rmsgh7000 <rmsgh70000@gmail.com>
2023-05-01 15:37:46 +0900

    :refactor: Do fizzbuzz with 1 if statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,5 @@
-for i in range(1,20):
-    if i%15==0:
-	print('fizzbuzz')
-    elif i%3==0:
-	print('fizz')
-    elif 1%5 == 0 :
-	print('buzz')
-    else
-	print(f'{f}')
-
-print('It works')
+for i in range(1,30+1):
+    if i%3==0 or i%5==0:
+        print('fizz'*(i%3==0)+'buzz'*(i%5==0))
+    else:
+        print(f'{i}')
