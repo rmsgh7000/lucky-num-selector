cbb4723ac5ac68a66e179fd7e3ab153191ac4565
rmsgh7000 <rmsgh70000@gmail.com>
2023-05-01 15:30:31 +0900

    fix: Replac i to j, Adjust End num 20 to 16

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,10 @@
-for i in range(1,20):
-    if i%15==0:
-	print('fizzbuzz')
-    elif i%3==0:
-	print('fizz')
-    elif 1%5 == 0 :
-	print('buzz')
-    else
-	print(f'{f}')
+for j in range(1,16+1):
+    if j%15==0:
+        print('fizzbuzz')
+    elif j%3==0:
+        print('fizz')
+    elif j%5==0:
+        print('buzz')
+    else:
+        print(f'{j}')
 
-print('It works')
